1fe239c607b12a73b512c9de01e0a22682577c31
Student <Student@apples-MacBook-Air-3.local>
2016-11-17 10:19:58 +0530

    Initial Commit

---
+++ NSOPeration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSOPeration
+//
+//  Created by Student on 17/11/16.
+//  Copyright (c) 2016 Abhijit.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
