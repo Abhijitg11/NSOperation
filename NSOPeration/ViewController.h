830c1fecc02a7dfad9b1f8e59a232d5e3404b25e
Student <Student@apples-MacBook-Air-3.local>
2016-11-18 10:22:29 +0530

    this is 1st commit in  master

--- NSOPeration/ViewController.h
+++ NSOPeration/ViewController.h
@@ -10,6 +10,16 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *indicator;
+@property (strong, nonatomic) IBOutlet UIImageView *imagview;
+- (IBAction)download:(id)sender;
+@property(nonatomic,retain)NSOperationQueue *queue;
+@property (strong, nonatomic) IBOutlet UIImageView *imageview2;
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *indicator2;
+
+
+
+
 
 @end
 
--- NSOPeration/ViewController.m
+++ NSOPeration/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _queue=[[NSOperationQueue alloc]init];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +26,37 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)download:(id)sender {
+
+    NSString *imgpath1=@"http://www.planwallpaper.com/static/images/desktop-year-of-the-tiger-images-wallpaper.jpg";
+    [_indicator startAnimating];
+    
+NSBlockOperation *op1=[NSBlockOperation blockOperationWithBlock:^{
+    NSURL *url=[NSURL URLWithString:imgpath1];
+    NSData *data=[NSData dataWithContentsOfURL:url];
+    UIImage *img=[UIImage imageWithData:data];
+    [[NSOperationQueue mainQueue]addOperationWithBlock:^{
+        _imagview.image=img;
+        [_indicator stopAnimating];
+    }];
+}];
+    [_queue addOperation:op1];
+    
+    NSString *imgpath2=@"http://www.planwallpaper.com/static/images/desktop-year-of-the-tiger-images-wallpaper.jpg";
+    [_indicator2 startAnimating];
+    
+    NSBlockOperation *op2=[NSBlockOperation blockOperationWithBlock:^{
+        NSURL *url=[NSURL URLWithString:imgpath2];
+        NSData *data=[NSData dataWithContentsOfURL:url];
+        UIImage *img2=[UIImage imageWithData:data];
+        [[NSOperationQueue mainQueue]addOperationWithBlock:^{
+            _imageview2.image=img2;
+            [_indicator2 stopAnimating];
+        }];
+    }];
+    [_queue addOperation:op2];
+    
+
+
+}
 @end
